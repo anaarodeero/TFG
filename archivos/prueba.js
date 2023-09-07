c77fcca9b4f04c84e8eaddc95218fdee7800db7d
anaarodeero <38472294+anaarodeero@users.noreply.github.com>
2023-09-07 12:31:17 +0200

    Delete archivos directory

--- archivos/prueba.js
+++
@@ -1,5 +0,0 @@
-db.getCollection('alimentos').insert({"idAlimento": NumberInt(653),"alimento": "Jamón Ibérico de Bellota","estado": "","categoria": "CARNE","cal": 101,"pr": 9.96,"gr": 6.61,"hc": 0.16})
-db.getCollection('alimentos').insert({"idAlimento": NumberInt(654),"alimento": "Pulpo","estado": "","categoria": "PESCADO","cal": 82,"pr": 14.91,"gr": 1.04,"hc": 2.2})
-db.getCollection('alimentos').insert({"idAlimento": NumberInt(655),"alimento": "Vino Pedro Ximenez","estado": "","categoria": "","cal": 160,"pr": 0.1,"gr": 0,"hc": 13})
-db.getCollection('alimentos').insert({"idAlimento": NumberInt(656),"alimento": "Paprika","estado": "","categoria": "","cal": 289,"pr": 14.76,"gr": 12.95,"hc": 55.74})
-db.getCollection('alimentos').insert({"idAlimento": NumberInt(657),"alimento": "Pipas de girasol","estado": "","categoria": "FRUTOS SECOS","cal": 602,"pr": 25.8,"gr": 51,"hc": 3.8})
